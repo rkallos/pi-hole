9b5a2b3e4cfa517f75a545652e60e0ae9d9817f8
Richard Kallos <rkallos@gmail.com>
2016-01-23 05:22:38 +0000

    Migrated lrs.php to use SQLite3

--- advanced/Scripts/lrs.php
+++ advanced/Scripts/lrs.php
@@ -15,8 +15,7 @@ $pie_chart_regexp_adver = ": /etc/pihole/gravity.list";
 //$db_name = "charts";
 //$db_user = "root";
 //$db_pass = "hi";
-$db_file = "/usr/local/bin/piholestats.db";
-$db = new SQLite3($db_file);
+$db_file = "piholestats.db";
 
 //---------------------------------------------------------------------------------------------------
 //logging
@@ -49,11 +48,11 @@ function parse_top_chart_advertiser($file_line) {
 	return $adver;
 }
 
-function update_top_chart_stats($db_conn, $top_chart_array, $pie_chart_date) {
+function update_top_chart_stats($db, $top_chart_array, $pie_chart_date) {
 	$top_sorted = array_count_values($top_chart_array);
 	foreach ($top_sorted as $top_id => $value) {
 		$sql_replace_top_chart_stats = "REPLACE INTO top_chart_stats VALUES ('$pie_chart_date', '$top_id', $value)";
-		if (!SQLite3::query($db_conn, $sql_replace_top_chart_stats)) {
+		if (!$db->query($sql_replace_top_chart_stats)) {
 			errorlog("SQL query failed for some reason: $sql_replace_top_chart_stats");
 		}else{
 			debuglog("SQL query successfully executed: $sql_replace_top_chart_stats");
@@ -61,18 +60,18 @@ function update_top_chart_stats($db_conn, $top_chart_array, $pie_chart_date) {
 	}
 }
 
-function update_pie_chart_stats($db_conn, $pie_chart_date, $pie_chart_count_query, $pie_chart_count_adver) {
+function update_pie_chart_stats($db, $pie_chart_date, $pie_chart_count_query, $pie_chart_count_adver) {
 	//daily insert
-	$sql_insert_pie_chart_stats = "INSERT IGNORE INTO pie_chart_stats VALUES ('$pie_chart_date', $pie_chart_count_query, $pie_chart_count_adver)";
-	if (!mysqli_query($db_conn, $sql_insert_pie_chart_stats)) {
+	$sql_insert_pie_chart_stats = "INSERT OR IGNORE INTO pie_chart_stats VALUES ('$pie_chart_date', $pie_chart_count_query, $pie_chart_count_adver)";
+	if (!$db->query($sql_insert_pie_chart_stats)) {
 		errorlog("SQL query failed for some reason: $sql_insert_pie_chart_stats");
 	}else{
 		debuglog("SQL query successfully executed: $sql_insert_pie_chart_stats");
 	}
 	//total calc
 	$sql_select_pie_chart_stats  = "SELECT SUM(query_cnt) AS q_cnt, SUM(adver_cnt) AS a_cnt FROM pie_chart_stats WHERE insert_date != '0000-00-00'";
-	if ($res_select_pie_chart_stats = mysqli_query($db_conn, $sql_select_pie_chart_stats)) {
-		$arr_select_pie_chart_stats = mysqli_fetch_array($res_select_pie_chart_stats);
+	if ($res_select_pie_chart_stats = $db->query($sql_select_pie_chart_stats)) {
+		$arr_select_pie_chart_stats = $res_select_pie_chart_stats->fetchArray();
 		$total_query = $arr_select_pie_chart_stats['q_cnt'];
 		$total_adver = $arr_select_pie_chart_stats['a_cnt'];
 	}else{
@@ -80,7 +79,7 @@ function update_pie_chart_stats($db_conn, $pie_chart_date, $pie_chart_count_quer
 	}
 	//total update
 	$sql_update_pie_chart_stats = "UPDATE pie_chart_stats SET query_cnt = $total_query, adver_cnt = $total_adver WHERE insert_date = '0000-00-00'";
-	if (!mysqli_query($db_conn, $sql_update_pie_chart_stats)) {
+	if (!$db->query($sql_update_pie_chart_stats)) {
 		errorlog("SQL query failed for some reason: $sql_update_pie_chart_stats");
 	}else{
 		debuglog("SQL query successfully executed: $sql_update_pie_chart_stats");
@@ -123,20 +122,17 @@ debuglog("Total amount of 'adver' regexp for ".$pie_chart_date." == ".$pie_chart
 //---------------------------------------------------------------------------------------------------
 //mysql
 
-debuglog("** Trying to connect to database with db_host = $db_host");
-debuglog("** Trying to connect to database with db_user = $db_user");
-debuglog("** Trying to connect to database with db_pass = __skipped");
-debuglog("** Trying to connect to database with db_name = $db_name");
-if ($db_conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name)) {
+debuglog("** Trying to connect to Sqlite3 database");
+if ($db = new SQLite3($db_file)) {
 	debuglog("Database connection succesfully established");
 }else{
 	errorlog("Could not connect to database, check configured DB credentials");
 }
 
 debuglog("--- Going to populate data into pie_chart_stats");
-update_pie_chart_stats($db_conn, $pie_chart_date, $pie_chart_count_query, $pie_chart_count_adver);
+update_pie_chart_stats($db, $pie_chart_date, $pie_chart_count_query, $pie_chart_count_adver);
 debuglog("--- Data populated into pie_chart_stats");
 debuglog("--- Going to populate data into top_chart_stats");
-update_top_chart_stats($db_conn, $top_chart_array, $pie_chart_date);
+update_top_chart_stats($db, $top_chart_array, $pie_chart_date);
 debuglog("--- Data populated into top_chart_stats");
 debuglog("Script is finished");
